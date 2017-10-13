e0023219b0892638995fee4520e9a2b64fede568
Marco <m.barbone@outlook.it>
2017-10-13 18:21:17 +0200

    first commit

---
+++ src/main/java/it/cnr/isti/pad/mb/FindWord.java
@@ -0,0 +1,13 @@
+package it.cnr.isti.pad.mb;
+
+/**
+ * Hello world!
+ *
+ */
+public class FindWord
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
